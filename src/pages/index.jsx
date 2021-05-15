1077c1f3593e2c651d791aaf89955b3a3fbfe188
Jou21 <jvsb21@gmail.com>
2021-05-15 18:11:08 -0300

    segundo commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <h1>Home eae</h1>
       <Link href="/sobre">
         <a>Ir para Sobre</a>
       </Link>
