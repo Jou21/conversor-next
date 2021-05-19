e376ef6f951c74afff3b78f2b5147a57d068b1eb
Jou21 <jvsb21@gmail.com>
2021-05-19 19:55:11 -0300

    tratando 18

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -39,8 +39,17 @@ export default function Home({ propriedades }) {
 export const getStaticProps = async () => {
   let arrayDados = [];
   var i;
-  for (i = 1; i < 4; i++) {
+  for (i = 1; i < 2; i++) {
     const urlCoingecko = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=250&page=${i}&sparkline=false`;
+
+    const response = await fetch(urlCoingecko);
+    const data = await response.json();
+
+    arrayDados = arrayDados.concat(data);
+  }
+
+  for (i = 1; i < 2; i++) {
+    const urlCoingecko = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=${i}&sparkline=false`;
 
     const response = await fetch(urlCoingecko);
     const data = await response.json();
--- src/pages/moedas/[id].jsx
+++ src/pages/moedas/[id].jsx
@@ -13,19 +13,34 @@ export default function Moeda({ parDeMoeda }) {
 
 export const getStaticPaths = async () => {
   let arrayDados = [];
+  let arrayDados2 = [];
+
   var i;
-  for (i = 1; i < 4; i++) {
+  for (i = 1; i < 2; i++) {
     const urlCoingecko = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=250&page=${i}&sparkline=false`;
 
     const response = await fetch(urlCoingecko);
     const data = await response.json();
 
     arrayDados = arrayDados.concat(data);
+
+    const urlCoingecko2 = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=${i}&sparkline=false`;
+
+    const response2 = await fetch(urlCoingecko2);
+    const data2 = await response2.json();
+
+    arrayDados2 = arrayDados2.concat(data2);
   }
 
-  const paths = arrayDados.map((moeda) => {
+  let paths1 = arrayDados.map((moeda) => {
     return { params: { id: `${moeda.symbol.toUpperCase()}BRL` } };
   });
+
+  let paths2 = arrayDados2.map((moeda) => {
+    return { params: { id: `${moeda.symbol.toUpperCase()}USD` } };
+  });
+
+  const paths = paths1.concat(paths2);
 
   /* const paths = [
     {
