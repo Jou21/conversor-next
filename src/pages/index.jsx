cb194eca8f080c948c9809e922617c90a802652b
Jou21 <jvsb21@gmail.com>
2021-05-19 16:46:46 -0300

    tratando 14

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,13 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
 import Grafico from "../components/Grafico";
 
-import { server } from "../config";
-
-import React, { Component } from "react";
 import Select from "react-select";
 
 import Link from "next/link";
 
 export default function Home({ propriedades }) {
-  const { isFallback } = useRouter();
-
-  if (isFallback) {
-    return <p>Carregando....</p>;
-  }
-
   const options = [];
 
-  propriedades.data.map((moeda) => {
+  propriedades.map((moeda) => {
     const array = {
       value: moeda.name,
       label: (
@@ -39,8 +28,8 @@ export default function Home({ propriedades }) {
 
   return (
     <div className="container">
-      <h1>eae {propriedades.data[0].symbol}</h1>
-      <Grafico simbolo={`${propriedades.data[0].symbol.toUpperCase()}BRL`} />
+      <h1>eae {propriedades[0].symbol}</h1>
+      <Grafico simbolo={`${propriedades[0].symbol.toUpperCase()}BRL`} />
 
       <Select options={options} />
     </div>
@@ -48,44 +37,16 @@ export default function Home({ propriedades }) {
 }
 
 export const getStaticProps = async () => {
-  //const urlVercel = process.env.URL_VERCEL;
-  //const urlLocalHost = process.env.URL_LOCAL_HOST;
-  //const urlCoingecko = `${server}/api/paresBRL/`;
-
   let arrayDados = [];
-  var i;
-  for (i = 1; i < 2; i++) {
-    const urlCoingecko = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=250&page=${i}&sparkline=false`;
 
-    const response = await fetch(urlCoingecko);
-    const data = await response.json();
+  const urlCoingecko = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=250&page=1&sparkline=false`;
 
-    arrayDados = arrayDados.concat(data);
-  }
-
-  /* let data = [];
-  let error = "";
-  let paths = [];
-
-  try {
-    const res = await fetch(urlCoingecko, {
-      method: "GET",
-      headers: {
-        // update with your user-agent
-        "User-Agent":
-          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
-        Accept: "application/json; charset=UTF-8",
-      },
-    });
-
-    data = await res.json();
-  } catch (e) {
-    error = e.toString();
-  } */
+  const response = await fetch(urlCoingecko);
+  const data = await response.json();
 
   return {
     props: {
-      propriedades: arrayDados,
+      propriedades: data,
     },
     revalidate: 10000,
   };
--- src/pages/moedas/[id].jsx
+++ src/pages/moedas/[id].jsx
@@ -1,17 +1,6 @@
-import { useRouter } from "next/router";
 import Grafico from "../../components/Grafico";
 
-import { server } from "../../config";
-
-import axios from "axios";
-
 export default function Moeda({ parDeMoeda }) {
-  const { isFallback } = useRouter();
-
-  if (isFallback) {
-    return <p>Carregando...</p>;
-  }
-
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>
@@ -23,40 +12,6 @@ export default function Moeda({ parDeMoeda }) {
 }
 
 export const getStaticPaths = async () => {
-  //const urlVercel = process.env.URL_VERCEL;
-  //const urlLocalHost = process.env.URL_LOCAL_HOST;
-  //const urlCoingecko = `${server}/api/todosOsPares/`;
-
-  /* const response = await fetch(urlCoingecko);
-  const data = await response.json(); */
-
-  /* let data = [];
-  let error = "";
-  let paths = [];
-
-  try {
-    const res = await fetch(urlCoingecko, {
-      method: "GET",
-      headers: {
-        // update with your user-agent
-        "User-Agent":
-          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
-        Accept: "application/json; charset=UTF-8",
-      },
-    });
-
-    data = await res.json();
-    paths = data["date"].map((moeda) => {
-      return { params: { id: moeda.parDeMoeda } };
-    });
-  } catch (e) {
-    error = e.toString();
-  } */
-
-  /* const paths = data["date"].map((moeda) => {
-    return { params: { id: moeda.parDeMoeda } };
-  }); */
-
   const paths = [
     {
       params: { id: "ETHBRL" },
