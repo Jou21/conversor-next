102877d80b3fd7a6c8a51ebfa5e4d041d8e19e73
Jou21 <jvsb21@gmail.com>
2021-05-19 09:30:26 -0300

    tratando urls

--- src/pages/moedas/[id].jsx
+++ src/pages/moedas/[id].jsx
@@ -5,7 +5,7 @@ export default function Moeda({ parDeMoeda }) {
   const { isFallback } = useRouter();
 
   if (isFallback) {
-    return <p>Carregando....</p>;
+    return <p>Carregando...</p>;
   }
 
   return (
