c034aa310f03e491e6a918febaada420d864da91
Jou21 <jvsb21@gmail.com>
2021-05-23 15:52:30 -0300

    tratando 21

--- src/components/Grafico/index.jsx
+++ src/components/Grafico/index.jsx
@@ -1,7 +1,56 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState, useEffect } from "react";
 
-export default class Grafico extends React.PureComponent {
+export default function Grafico(props) {
+  let url = `https://br.tradingview.com/symbols/${props.simbolo}`;
+
+  const ref = React.useRef(null);
+
+  useEffect(() => {
+    let scriptNovo = document.createElement("script");
+
+    scriptNovo.onload = function () {
+      new TradingView.widget({
+        width: 980,
+        height: 610,
+        symbol: `${props.simbolo}`,
+        interval: "D",
+        timezone: "America/Sao_Paulo",
+        theme: "light",
+        style: "1",
+        locale: "br",
+        toolbar_bg: "#f1f3f6",
+        enable_publishing: false,
+        hide_side_toolbar: false,
+        allow_symbol_change: true,
+        show_popup_button: true,
+        popup_width: "1200",
+        popup_height: "850",
+        container_id: "tradingview_823e3",
+      });
+    };
+
+    scriptNovo.src = "https://s3.tradingview.com/tv.js";
+    scriptNovo.async = true;
+    scriptNovo.innerHTML = ref.current.appendChild(scriptNovo);
+  }, [url]);
+
+  return (
+    <div className="tradingview-widget-container" ref={ref}>
+      <div className="tradingview-widget-container__widget"></div>
+      <div id="tradingview_823e3"></div>
+      <div className="tradingview-widget-copyright">
+        powered by
+        <a href={url} rel="noopener" target="_blank">
+          <span className="blue-text"> TradingView</span>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+//import ReactDOM from "react-dom";
+
+/* export default class Grafico extends React.PureComponent {
   constructor(props) {
     super(props);
     this._ref = React.createRef();
@@ -13,9 +62,9 @@ export default class Grafico extends React.PureComponent {
     };
   }
   componentDidMount() {
-    const { simbolo } = this.state;
+    let { simbolo } = this.state;
 
-    const scriptNovo = document.createElement("script");
+    let scriptNovo = document.createElement("script");
 
     scriptNovo.onload = function () {
       new TradingView.widget({
@@ -61,4 +110,4 @@ export default class Grafico extends React.PureComponent {
       </div>
     );
   }
-}
+} */
