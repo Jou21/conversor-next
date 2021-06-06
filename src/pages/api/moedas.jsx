b298877e751e5b79d0520e72fb5dc233c4cff272
Jou21 <jvsb21@gmail.com>
2021-06-06 15:12:33 -0300

    tratando 78

--- src/pages/api/moedas.jsx
+++ src/pages/api/moedas.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 async function moedas(req, res) {
   var arrayDados = [];
   var i;
-  for (i = 1; i < 20; i++) {
+  for (i = 1; i < 25; i++) {
     const response = await axios.get(
       `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=${i}&sparkline=false`
     );
