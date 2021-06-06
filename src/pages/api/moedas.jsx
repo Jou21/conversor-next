c4062ebee0cb9fe1648ce971a899214371ff7072
Jou21 <jvsb21@gmail.com>
2021-06-06 15:04:23 -0300

    tratando 74

--- src/pages/api/moedas.jsx
+++ src/pages/api/moedas.jsx
@@ -1,7 +1,21 @@
+import axios from "axios";
+
 async function moedas(req, res) {
   var arrayDados = [];
-
   var i;
+  for (i = 1; i < 3; i++) {
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=${i}&sparkline=false`
+    );
+    const data = await response.data;
+    arrayDados = arrayDados.concat(data);
+  }
+  /* const response = await axios.get(
+    "https://api.tvmaze.com/search/shows?q=batman"
+  );
+  const data = await response.data; */
+
+  /* var i;
   for (i = 1; i < 3; i++) {
     const urlImagensCoingecko = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=${i}&sparkline=false`;
 
@@ -9,12 +23,12 @@ async function moedas(req, res) {
     const dataImagensCoingecko = await responseUrlImagensCoingecko.json();
 
     arrayDados = arrayDados.concat(dataImagensCoingecko);
-  }
+  } */
 
   res.setHeader("Cache-Control", "s-maxage=10000, stale-while-revalidate");
 
   res.json({
-    date: arrayDados,
+    data: arrayDados,
   });
 }
 
