2816a8a8a8f63f137b450922717327aa262fa64d
Jou21 <jvsb21@gmail.com>
2021-06-06 14:02:07 -0300

    tratando 70

--- src/pages/api/index.jsx
+++ src/pages/api/index.jsx
@@ -1,49 +1,20 @@
 async function api(req, res) {
-  let arrayDadosBRL = [];
+  var arrayDados = [];
 
   var i;
-  for (i = 1; i < 2; i++) {
-    const urlCoingecko = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=250&page=${i}&sparkline=false`;
+  for (i = 1; i < 35; i++) {
+    const urlImagensCoingecko = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=${i}&sparkline=false`;
 
-    const response = await fetch(urlCoingecko);
-    const data = await response.text();
+    const responseUrlImagensCoingecko = await fetch(urlImagensCoingecko);
+    const dataImagensCoingecko = await responseUrlImagensCoingecko.json();
 
-    arrayDadosBRL = arrayDadosBRL.concat(data);
+    arrayDados = arrayDados.concat(dataImagensCoingecko);
   }
 
-  const urlCotacoesFiat = "https://api.hgbrasil.com/finance?key=6fe79203";
-
-  const responseCotacoesFiat = await fetch(urlCotacoesFiat);
-  const dataCotacoesFiat = await responseCotacoesFiat.text();
-
-  const cotacoesFiat = dataCotacoesFiat.results.currencies;
-
-  const urlImgMoedaCrypto = `https://api.coinranking.com/v2/search-suggestions?query=ethereum`;
-
-  const responseUrlImgMoedaCrypto = await fetch(urlImgMoedaCrypto);
-  const dataUrlImgMoedaCrypto = await responseUrlImgMoedaCrypto.text();
-
-  const stringImgMoedaCrypto = dataUrlImgMoedaCrypto.data.coins[0].iconUrl;
-
-  //const stringImgEUR = "https://cdn.coinranking.com/fz3P5lsJY/eur.svg"
-
-  //Usar para buscar imagem caso mudem a api
-  /* 
-  const urlImgMoedaFiat = `https://api.coinranking.com/v2/reference-currencies`;
-  const responseUrlImgMoedaFiat = await fetch(urlImgMoedaFiat);
-  const dataUrlImgMoedaFiat = await responseUrlImgMoedaFiat.json();
-
-  const stringImgMoedaFiat = dataUrlImgMoedaFiat;
-
-  console.log(stringImgMoedaFiat);
-  */
-
-  res.setHeader("Cache-Control", "s-maxage=10000, stale-while-revalidate");
+  res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
 
   res.json({
-    propriedades: arrayDadosBRL,
-    cotacoesFiat: cotacoesFiat,
-    stringImgMoedaCrypto,
+    date: arrayDados,
   });
 }
 
