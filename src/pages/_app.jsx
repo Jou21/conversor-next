b7f322ae199589b825ce93304afa045e143b310a
Jou21 <jvsb21@gmail.com>
2021-07-18 22:58:17 -0300

    tratando 101

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import CookieConsent, { Cookies } from "react-cookie-consent";
+import Link from "next/link";
+import styles from "../styles.module.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,6 +14,47 @@ function MyApp({ Component, pageProps }) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
       </Head>
+      <div
+        style={{
+          zIndex: "99",
+          position: "fixed",
+          right: "0",
+          bottom: "0",
+          justifyContent: "space-between",
+          width: "100%",
+          background: "white",
+        }}
+      >
+        <CookieConsent
+          style={{
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: "10px",
+            marginBottom: "14px",
+            marginRight: "40px",
+            marginLeft: "40px",
+          }}
+          disableStyles={true}
+          location="bottom"
+          buttonText="Compreendo!"
+          cookieName="myAwesomeCookieName2"
+          buttonClasses={styles.myHoveringButton}
+          expires={150}
+        >
+          <span style={{ color: "gray", fontSize: "15px" }}>
+            Este site usa cookies para garantir que você obtenha a melhor
+            experiência em nosso site. {"    "}
+          </span>
+
+          <Link href="/sobre">
+            <a style={{ textDecorationLine: "underline", fontSize: "15px" }}>
+              Leia mais
+            </a>
+          </Link>
+        </CookieConsent>
+      </div>
+
       <Component {...pageProps} />
     </>
   );
